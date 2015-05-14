15ac1d994ef6acffce30103bf8b0b3e280a2111f
Jordan Zucker <jordan.zucker@gmail.com>
2015-05-14 16:37:29 -0700

    Removed unstable cache code handling.

--- PubNub/PubNub/PubNub/Network/PNReachability.m
+++ PubNub/PubNub/PubNub/Network/PNReachability.m
@@ -643,19 +643,6 @@ void reachabilityContextInformationReleaseCallBack(const void *info) {
                                                                        cachePolicy:NSURLRequestReloadIgnoringLocalCacheData
                                                                    timeoutInterval:kPNReachabilityOriginLookupTimeout];
                 NSData *downloadedTimeTokenData = [NSURLConnection sendSynchronousRequest:timeTokenRequest returningResponse:&response error:&requestError];
-                #if __IPHONE_OS_VERSION_MIN_REQUIRED
-                BOOL hasPlaceForCache = [[NSURLCache sharedURLCache] memoryCapacity] > 0;
-                #else
-                BOOL hasPlaceForCache = ([[NSURLCache sharedURLCache] memoryCapacity] > 0 ||
-                                         [[NSURLCache sharedURLCache] diskCapacity] > 0);
-                #endif
-                if (hasPlaceForCache) {
-                    
-                    if ([[NSURLCache sharedURLCache] cachedResponseForRequest:timeTokenRequest]) {
-                        
-                        [[NSURLCache sharedURLCache] removeCachedResponseForRequest:timeTokenRequest];
-                    }
-                }
 
                 dispatch_queue_t targetQueue = [strongSelf pn_privateQueue];
                 if (targetQueue != NULL) {
