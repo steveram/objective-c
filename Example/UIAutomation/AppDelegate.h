8202bd56d155b28b216d131ec1e708a1ae5ab628
Jordan Zucker <jordan.zucker@gmail.com>
2015-08-26 11:40:02 -0700

    Added UIAutomation app

---
+++ Example/UIAutomation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIAutomation
+//
+//  Created by Jordan Zucker on 8/26/15.
+//  Copyright (c) 2015 Jordan Zucker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Example/UIAutomation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIAutomation
+//
+//  Created by Jordan Zucker on 8/26/15.
+//  Copyright (c) 2015 Jordan Zucker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
